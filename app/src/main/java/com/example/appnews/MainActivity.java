beb5e562755d175942a70516382e425337e84d8a
pratikSroy <pratik.sr99@gmail.com>
2022-03-21 13:23:03 +0530

    search bar

--- app/src/main/java/com/example/appnews/MainActivity.java
+++ app/src/main/java/com/example/appnews/MainActivity.java
@@ -8,16 +8,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.appnews.Models.NewsApiResponse;
 import com.example.appnews.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener{
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog dialog;
+    Button b1,b2,b3,b4,b5,b6,b7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,21 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
         dialog = new ProgressDialog(this);
         dialog.setTitle("Fetching News Articles");
         dialog.show();
+
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
 
         RequestManager manager = new RequestManager(this );
         manager.getNewsHeadlines(listener, "general", null);
@@ -59,5 +77,15 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
     public void OnNewsClicked(NewsHeadlines headlines) {
         startActivity(new Intent(MainActivity.this, DetailsActivity.class)
         .putExtra("data", headlines));
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String category = button.getText().toString();
+        dialog.setTitle("Fetching news articles of "+category);
+        dialog.show();
+        RequestManager manager = new RequestManager(this );
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
